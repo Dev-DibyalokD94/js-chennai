2c9052da3efe714d620aa57042bdafeb2891f81c
Dev-DibyalokD94 <dibyalokwork@gmail.com>
2024-08-22 17:17:48 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+// let score = undefined
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+
+// console.log(valueInNumber); //NaN -> Not a Number
+
+/*
+number : 33 -> 33
+33ABC -> NaN
+true -> 1
+false -> 0
+null -> 0
+undefined -> NaN
+
+
+*/
+// 
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn); // op -> true
+
+/*
+1 -> true
+0 -> false
+"" -> false
+"dd" -> true
+
+
+*/
+
+
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
