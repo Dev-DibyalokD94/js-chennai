0997b598d216f57506f40991c0f656f4c008161d
Dev-DibyalokD94 <dibyalokwork@gmail.com>
2024-08-22 17:38:21 +0000

    Conversion and Operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -39,6 +39,65 @@ let someNumber = 33
 
 let StringNumber = String(someNumber)
 
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+
+//***************************************operations**********************************
+
+
+let value = 3
+let negValue = -value
+
+
+// console.log(negValue);
+// console.log(typeof negValue);
+
+let str1 = "Hello"
+let str2 = " Dibyalok"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true) //true
+console.log(+true); //1
+// console.log(true+);//error
+
+console.log(+""); //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+console.log([num1, num2, num3]);
+
+
+let gameCounter = 100
+gameCounter++ //postfix
+console.log(gameCounter); //101
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
