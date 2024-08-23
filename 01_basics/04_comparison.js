94fdf6f143878b72a0bd0ea931effb402cba1bf4
Dev-DibyalokD94 <dibyalokwork@gmail.com>
2024-08-23 04:18:53 +0000

    Comparison checks

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+//problem comes when we don't compare same datatype.
+
+console.log("2" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+//Strict check ===
+
+console.log("2" === 2); //false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
