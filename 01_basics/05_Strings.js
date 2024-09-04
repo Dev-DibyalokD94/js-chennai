3a76dc7b1a1ce3deecca0d48061986f9c9a57f61
Dev-DibyalokD94 <dibyalokwork@gmail.com>
2024-09-04 11:35:22 +0000

    Strings

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,19 @@
+// Back ticks `` for String Interpolation-> creating palce holders
+
+let name = "Dibyalok"
+let count = 747
+
+console.log(`Hello my name is ${name} and my fav number is ${count}`);
+
+// the main advantage is -> we can write the messages on the go
+
+console.log(name);
+console.log(typeof name);
+
+let newName = new String("DD747")
+
+console.log(newName);
+console.log(typeof newName);
+
+console.log(newName[0]);
+console.log(newName.__proto__);
