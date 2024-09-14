15a3c443a89fb994e1a592526e9968d1a4e1ddc2
Dev-DibyalokD94 <dibyalokwork@gmail.com>
2024-09-14 14:04:15 +0000

    Math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,17 +1,25 @@
 const score = 400
 
-console.log(score);
+// console.log(score);
 
 const balance = new Number(1000)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString());
-console.log(balance.toFixed(2));
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
 
 const otherNumber = 413.9966
-console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(4));
 
 const hundreds = 10000000
-console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ************************ Maths
+
+
+console.log(Math.random()*10)
+
+
+
 
 
